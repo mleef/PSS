b701a41cca4b8984563895b5241af58880acf730
Marc Leef <mleef@uoregon.edu>
2014-08-06 17:19:10 -0700

    In the process of enabling Node to handle simultaneous users

--- web/server.js
+++ web/server.js
@@ -25,12 +25,11 @@ var web ='/Users/marc_leef/Desktop/Work/PSS/web/'
 var fileNames =[]
 var chosenDesign = ""
 var sequenceText = ""
+var jobQueue = {}
 
-var processing = false
 
 
 // Selected design database
-cur_db = ""
 
 // JSON variable to be passed to client based on program progression		
 var status = {"step" : "(1/4) Recieving Data..."}
@@ -54,45 +53,45 @@ var databaseSelector = function (num) {
 	switch(num) {
 		case 1:
 			chosenDesign = 'HuEx-1_0-st'
-			cur_db = probe_db + 'HuEx-1_0-st'
+			return probe_db + 'HuEx-1_0-st'
 			break
 		case 2:
 			chosenDesign = 'MouseEx-1_0-st'
-			cur_db = probe_db + 'MouseEx-1_0-st'
+			return probe_db + 'MouseEx-1_0-st'
 			break
 		case 3:
 			chosenDesign = 'RatEx-2_0-st'
-			cur_db = probe_db + 'RatEx-1_0-st'
+			return probe_db + 'RatEx-1_0-st'
 			break
 		case 4:
 			chosenDesign = 'HuGene-1_0-st'
-			cur_db = probe_db + 'HuGene-1_0-st'
+			return probe_db + 'HuGene-1_0-st'
 			break
 		case 5:
 			chosenDesign = 'MouseGene-1_0-st'
-			cur_db = probe_db + 'MouseGene-1_0-st'
+			return probe_db + 'MouseGene-1_0-st'
 			break
 		case 6:
 			chosenDesign = 'RatGene-1_0-st'
-			cur_db = probe_db + 'RatGene-1_0-st'
+			return probe_db + 'RatGene-1_0-st'
 			break
 
 		case 712:
 			chosenDesign = 'HuGene-2_0-st'
-			cur_db = probe_db + 'HuGene-2_0-st'
+			return probe_db + 'HuGene-2_0-st'
 			break
 		case 715:
 			chosenDesign = 'MouseGene-2_0-st'
-			cur_db = probe_db + 'MouseGene-2_0-st'
+			return probe_db + 'MouseGene-2_0-st'
 			break
 		case 718:
 			chosenDesign = 'RatGene-2_0-st'
-			cur_db = probe_db + 'RatGene-2_0-st'
+			return probe_db + 'RatGene-2_0-st'
 			break
 			
 		case 725:
 			chosenDesign = 'HTA-2_0'
-			cur_db = probe_db + 'HTA-2_0'
+			return probe_db + 'HTA-2_0'
 			break
 	}
 
@@ -100,7 +99,8 @@ var databaseSelector = function (num) {
 
 // Core of the program execution
 // BLAST query sequence -> Transform output into HTML and tab spaced -> Process the output and join HTML files -> Return to client -> Cleanup used files
-var exonBlast = function(file_name, file1, html1, res) {
+var exonBlast = function(file_name, file1, html1, cur_db, req, res) {
+	jobQueue[req].status = {"step" : "(2/4) Querying sequences against probe database..."}
 	status = {"step" : "(2/4) Querying sequences against probe database..."}
 	var exonBlast = exec(blast + 'blastn' + ' -query ' + save + file_name + ' -db ' + cur_db + ' -num_threads 4 -outfmt 11 -word_size 7 -gapopen 5 -gapextend 2 -reward 1 -penalty -1 -out ' + file1, function (error, stdout, stderr) {
 			console.log(stdout)
@@ -108,6 +108,7 @@ var exonBlast = function(file_name, file1, html1, res) {
 			console.log(stderr)
 			console.log("3")
 			status = {"step" : "(3/4) Analyzing results..."}
+			jobQueue[req].status = {"step" : "(3/4) Analyzing results..."}
 			//sleep.sleep(2)
 			exec(scripts + 'format_blast.sh ' + file1 + ' ' + html1 + ' ' + queries + file_name + '.tsv' + ' ' + design, function (error, stdout, stderr) {
 				console.log(stdout)
@@ -115,6 +116,7 @@ var exonBlast = function(file_name, file1, html1, res) {
 				console.log(stderr)
 				console.log("4")
 				status = {"step" : "(4/4) Formatting output..."}
+				jobQueue[req].status = {"step" : "(4/4) Formatting output..."}
 				//sleep.sleep(1)
 				exec('python ' + scripts + 'join_blasts.py ' + html1 + ' ' + web + 'temp.html > ' + web + 'output.html' , function (error, stdout, stderr) {
 					console.log("5")
@@ -128,7 +130,7 @@ var exonBlast = function(file_name, file1, html1, res) {
 		    		res.end(html)
 		    		cleanup([queries + file_name + '.tsv', save + file_name, file1, html1, html1 + '.exon', html1 + '.gene', "output.html"])
 		    		cur_db = blast
-		    		processing = false
+		    		
 					})
 				})
 			})
@@ -145,84 +147,86 @@ app.use(express.static(__dirname));
 // set up our routes
 app.post("/upload", 
 	function (req, res) { 
-		if(!processing) {
-			var timestamp = Date.now() || +new Date()
-			var form = new formidable.IncomingForm();
-			
-		    form.parse(req, function(err, fields, files) {
-		    	databaseSelector(fields.q8_chipDesign)
-		    	sequenceText = fields.pf
-		    	res.writeHead(200, {'content-type': 'text/html'});
-		    });
-		 	
-		    form.on('progress', function(bytesReceived, bytesExpected) {
-		        var percent_complete = (bytesReceived / bytesExpected) * 100;
-		  		status = {"step" : "(1/4) Receiving Data...", "percentage" : percent_complete} 
-		    });
-		 
-		    form.on('error', function(err) {
-		        console.error(err);
-		    });
-		    
-		 
-		    form.on('end', function(fields, files) {
-		        /* New location of our uploaded file */
-		        var outfile = "temp" + timestamp
-		        var outpath = save + outfile
-		        console.log("1")
+		jobQueue[req] = {"res" : res, "status" : {"step" : "(1/4) Recieving Data..."}, "details" : {"files" : [], "design" : ""}}
+
+		var timestamp = Date.now() || +new Date()
+		var form = new formidable.IncomingForm();
+		var db = ""
+		
+	    form.parse(req, function(err, fields, files) {
+	    	db = databaseSelector(fields.q8_chipDesign)
+	    	jobQueue[req].details.design = db.replace(probe_db, "")
+	    	sequenceText = fields.pf
+	    	res.writeHead(200, {'content-type': 'text/html'});
+	    });
+	 	
+	    form.on('progress', function(bytesReceived, bytesExpected) {
+	        var percent_complete = (bytesReceived / bytesExpected) * 100;
+	  		jobQueue[req].status = {"step" : "(1/4) Receiving Data...", "percentage" : percent_complete} 
+	    });
+	 
+	    form.on('error', function(err) {
+	        console.error(err);
+	    });
+	    
+	 
+	    form.on('end', function(fields, files) {
+	        /* New location of our uploaded file */
+	        var outfile = "temp" + timestamp
+	        var outpath = save + outfile
+	        console.log("1")
 
 
-		        if(sequenceText.length > 0) {
-		        	exec("echo '" + sequenceText + "' >> " + outpath, function (error, stdout, stderr) {
-		     			fileNames.push("None")
-		        	})
-		        }
+	        if(sequenceText.length > 0) {
+	        	exec("echo '" + sequenceText + "' >> " + outpath, function (error, stdout, stderr) {
+	     			jobQueue[req].details.files.push("None")
+	        	})
+	        }
 
-		        else {
-			        /* Concatenate uploaded files then remove them */
-			        this.openedFiles.forEach( function (element) {
-			        	fileNames.push(element.name)
-			        	exec("cat " + element.path + " >> " + outpath, function (error, stdout, stderr) {
-				        	cleanup([element.path])
-				        })
+	        else {
+		        /* Concatenate uploaded files then remove them */
+		        this.openedFiles.forEach( function (element) {
+		        	fileNames.push(element.name)
+		        	jobQueue[req].details.files.push(element.name)
+		        	exec("cat " + element.path + " >> " + outpath, function (error, stdout, stderr) {
+			        	cleanup([element.path])
 			        })
-			    }
+		        })
+		    }
 
-		        sleep.sleep(1)
-		        //console.log('python ' + scripts + 'line_breaks.py ' + outpath + ' > results' + timestamp)
-		        exec('python ' + scripts + 'line_breaks.py ' + outpath + ' > ' + save + 'results' + timestamp, function (error, stdout, stderr) {
-		        	console.log("2")
-		        	cleanup([outpath])
+	        sleep.sleep(1)
+	        //console.log('python ' + scripts + 'line_breaks.py ' + outpath + ' > results' + timestamp)
+	        exec('python ' + scripts + 'line_breaks.py ' + outpath + ' > ' + save + 'results' + timestamp, function (error, stdout, stderr) {
+	        	console.log("2")
+	        	cleanup([outpath])
 
-			        outfile = "results" + timestamp
-			        outpath = save + outfile
-			    	
+		        outfile = "results" + timestamp
+		        outpath = save + outfile
+		    	
 
-			    	file1 =  results + outfile + '_exon_level_results'
-							
-					html1 = file1 + '.html'
+		    	file1 =  results + outfile + '_exon_level_results'
+						
+				html1 = file1 + '.html'
 
-					/* Analyze uploaded files */
-					exonBlast(outfile, file1, html1, res)
-					processing = true
-				})
-				
-			});
+				/* Analyze uploaded files */
+				exonBlast(outfile, file1, html1, db, req, res)
+			})
+			
+		});
 
-		}
 });
 
 //Route for status updates
 app.get("/status",
 	function (req, res) {
-		res.json(status)
+		res.json(jobQueue[req].status)
 });
 
 //Route for sending details about the user search to the client
 app.get("/details",
 	function (req, res) {
-		res.json({"files" : fileNames, "design" : chosenDesign})
-		fileNames = []
+		res.json(jobQueue[req].details)
+		delete jobQueue[req]
 });
 
 		
